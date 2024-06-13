46a36d5a6428a95600cee24b6566bfeab95eb8f9
Larissa Bianca <larissa_bianca@outlook.com>
2024-06-13 18:26:09 -0300

    unit tests

---
+++ test/main_test.py
@@ -0,0 +1,19 @@
+import pytest
+from app.main import get_deck_cards_data, get_deck_id_data, get_new_card, httpx
+
+def test_get_deck_id_data():
+    deck_id = get_deck_id_data()
+    assert isinstance(deck_id, str)
+    assert len(deck_id) > 0        
+
+def test_get_deck_cards_data():
+    deck_id = get_deck_id_data()
+    cards = get_deck_cards_data(deck_id)
+    assert isinstance(cards, list)
+    assert len(cards) == 2
+
+def test_get_new_card():
+    cards = get_new_card()
+    assert isinstance(cards, list)
+    assert len(cards) == 1
+
